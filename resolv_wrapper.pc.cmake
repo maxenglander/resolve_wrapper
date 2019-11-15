258335fb122022e7ee18101a8aefd59c3a414e29
Andreas Schneider <asn@samba.org>
2019-11-15 13:20:02 +0100

    cmake: Fix path of cmake config files and pkgconfig
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- resolv_wrapper.pc.cmake
+++ resolv_wrapper.pc.cmake
@@ -1,4 +1,4 @@
 Name: @PROJECT_NAME@
 Description: The resolv_wrapper library
 Version: @PROJECT_VERSION@
-Libs: @LIB_INSTALL_DIR@/@RESOLV_WRAPPER_LIB@
+Libs: @CMAKE_INSTALL_FULL_LIBDIR@/@RESOLV_WRAPPER_LIB@
